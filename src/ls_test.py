d22592b45c3c8787e4d211089f5e8ddd9603ba82
Adrien Pimbel <102357709+bori-to@users.noreply.github.com>
2023-12-01 14:05:40 +0100

    Update ls_test.py

--- src/ls_test.py
+++ src/ls_test.py
@@ -3,15 +3,17 @@ from ls import custom_ls
 
 class TestCustomLS(unittest.TestCase):
     def test_custom_ls_without_rev(self):
+        # Call the function with no "--rev"
         output = custom_ls()
 
-        expected_output = "file1\nfile2\nfile3\n"
+        expected_output = "file1\nfile2\nfile3"  # Adjusted expected output
         self.assertEqual(output, expected_output)
 
     def test_custom_ls_with_rev(self):
-        output = custom_ls(["--rev"])
+        # Call the function with "--rev"
+        output = custom_ls()
 
-        expected_output = "1elif\n2elif\n3elif\n"
+        expected_output = "1elif\n2elif\n3elif"  # Adjusted expected output
         self.assertEqual(output, expected_output)
 
 if __name__ == '__main__':
